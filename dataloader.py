cc0148a37717e242c07e7d01c21364e23ddfc977
NEOissss <neoixv@gmail.com>
2018-11-30 16:06:51 -0500

    add

--- dataloader.py
+++ dataloader.py
@@ -5,9 +5,11 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class NYUDv2Dataset(Dataset):
-    def __init__(self, file_path):
+    def __init__(self, file_path, cuda=True):
         super(NYUDv2Dataset, self).__init__()
         self.file_path = file_path
+        self.cuda = cuda
+
     def __getitem__(self, item):
         pass
 
@@ -16,3 +18,12 @@ class NYUDv2Dataset(Dataset):
 
     def _load_data(self):
         mat = hdf5storage.loadmat(self.file_path)
+        self.images = torch.FloatTensor(mat['images'])
+        self.depths = torch.FloatTensor(mat['depths'])
+        self.labels = torch.FloatTensor(mat['labels'])
+
+        if self.cuda:
+            self.images = self.images.cuda()
+            self.depths = self.depths.cuda()
+            self.labels = self.labels.cuda()
+
