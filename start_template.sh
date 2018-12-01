a1354ef2b10280fac0158fec4f7cf7f409669ce0
NEO <NEOissss@users.noreply.github.com>
2018-12-01 02:08:46 -0500

    fix

--- start_template.sh
+++ start_template.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-path='nyu_depth_v2_labeled.mat'
+path='--path nyu_depth_v2_labeled.mat'
 
 param=''
 #param='--n_param FCN8s-param-467382378'
 
 lr='--lr 0.001'
 decay='--decay 0.01'
-epoch='--epoch 10'
+epoch='--epoch 1'
 batch='--batch 128'
 verbose='--verbose 1'
 
