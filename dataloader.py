424988fb203c8d6cf77e2b53aa07f7e67981fdc6
NEO <NEOissss@users.noreply.github.com>
2018-11-30 23:23:27 -0500

    add

--- dataloader.py
+++ dataloader.py
@@ -1,29 +1,35 @@
 import hdf5storage
 import torch
-from torchvision.transforms import Normalize, Compose
+from torchvision.transforms import Normalize
 from torch.utils.data import Dataset, DataLoader
 
 
 class NYUDv2Dataset(Dataset):
-    def __init__(self, file_path, cuda=True):
+    def __init__(self, file_path, data_opts):
         super(NYUDv2Dataset, self).__init__()
         self.file_path = file_path
-        self.cuda = cuda
+        self.train = data_opts['train']
+        self.cut = data_opts['cut']
+        self.transform = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-    def __getitem__(self, item):
-        pass
+        # Load data
+        mat = hdf5storage.loadmat(self.file_path)
+        self.images = torch.FloatTensor(mat['images'])[self.cut[0], self.cut[1]]
+        self.depths = torch.FloatTensor(mat['depths'])[self.cut[0], self.cut[1]]
+        # self.labels = torch.FloatTensor(mat['labels'])[self.cut[0], self.cut[1]]
+
+        self.images = self.images.transpose(0, 3).transpose(1, 2).transpose(2, 3)
+        self.images = self.transform(self.images)
+
+        self.depths = self.depths.transpose(0, 2).transpose(1, 2).unsqueeze(1)
+        # self.labels = self.labels.transpose(0, 2).transpose(1, 2).unsqueeze(1)
 
     def __len__(self):
-        pass
+        return self.images.size(0)
 
-    def _load_data(self):
-        mat = hdf5storage.loadmat(self.file_path)
-        self.images = torch.FloatTensor(mat['images'])
-        self.depths = torch.FloatTensor(mat['depths'])
-        self.labels = torch.FloatTensor(mat['labels'])
+    def __getitem__(self, idx):
+        return self.images[[idx]], self.depths[[idx]]
 
-        if self.cuda:
-            self.images = self.images.cuda()
-            self.depths = self.depths.cuda()
-            self.labels = self.labels.cuda()
 
+if __name__ == '__main__':
+    data_opts = {'train': True, 'cut': [0, 500]}
