0a95daad3cbf651d90b435d1d76a66920e3b1f46
NEO <NEOissss@users.noreply.github.com>
2018-12-04 00:21:46 -0500

    modify conv layer

--- fcn.py
+++ fcn.py
@@ -85,17 +85,13 @@ class FCN8s(nn.Module):
         self.upscore3 = nn.ConvTranspose2d(output_size, output_size, 16, stride=8)
 
         self.final_score = nn.Sequential(
-            nn.Conv2d(1, 32, 3, padding=1),
+            nn.Conv2d(1, 32, 7, padding=3),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 64, 3, padding=1),
+            nn.Conv2d(32, 32, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 128, 3, padding=1),
+            nn.Conv2d(32, 32, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 64, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 32, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 1, 3, padding=1)
+            nn.Conv2d(32, 1, 3, padding=1),
         )
 
         self.init_params(pretrain=pretrain)
@@ -167,7 +163,7 @@ class FCN8s(nn.Module):
             torch.nn.init.kaiming_normal_(layer.weight.data)
             torch.nn.init.constant_(layer.bias.data, val=0)
 
-        for i in [0 ,2, 4, 6, 8, 10]:
+        for i in [0, 2, 4, 6]:
             torch.nn.init.kaiming_normal_(self.final_score[i].weight.data)
             torch.nn.init.constant_(self.final_score[i].bias.data, val=0)
 
